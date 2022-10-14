298fbee16b7014aad31168678d147685cbca0613
Andreas Poehlmann <andreas@poehlmann.io>
2022-10-14 13:31:21 +0200

    scikit_mol: add tests skeleton and restructure package metadata

---
+++ scikit_mol/__init__.py
@@ -0,0 +1,4 @@
+try:
+    from scikit_mol._version import __version__
+except ImportError:  # pragma: no cover
+    __version__ = "not-installed"
---
+++ tests/test_scikit_mol.py
@@ -0,0 +1,4 @@
+
+def test_load_data(data):
+    assert len(data) > 0
+
