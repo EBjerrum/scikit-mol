4e2cd681e5cf65a503a261510f33ec4413bb89b5
Esben Bjerrm <esben@odysseytx.com>
2022-10-15 13:27:22 +0000

    Refactored the descriptor transformer and made a demo

---
+++ notebooks/descriptor_demo.py
@@ -0,0 +1,39 @@
+# %% [markdown]
+# Demo of the descriptor module
+
+# %%
+from rdkit import Chem
+import numpy as np
+import matplotlib.pyplot as plt
+from scikit_mol.descriptors import Desc2DTransformer
+# %% [markdown]
+# What descriptors are available 
+
+descriptor = Desc2DTransformer()
+available_descriptors = descriptor.available_descriptors
+print(f"There are {len(available_descriptors)} available descriptors")
+
+# %% [markdown]
+# We can transform molecules to their descriptors
+
+# %%
+smiles_list = ["CCCC", "c1ccccc1"]
+mols = [Chem.MolFromSmiles(smiles) for smiles in smiles_list]
+
+features = descriptor.transform(mols)
+plt.plot(np.array(features).T)
+
+# %% [markdown]
+# If we only want some of them, we can specify that at descriptor creation time
+
+# %%
+some_descriptors = Desc2DTransformer(custom_desc_list=['HeavyAtomCount', 'FractionCSP3', 'RingCount', 'MolLogP', 'MolWt'])
+features = some_descriptors.transform(mols)
+plt.plot(np.array(features).T)
+
+# %% [markdown]
+# There is an error check of the specified descriptor list
+
+# %%
+some_faulty_descriptors = Desc2DTransformer(custom_desc_list=['Color', 'Icecream content', 'ChokolateDarkness', 'Content42', 'MolWt'])
+# %%
--- scikit_mol/descriptors.py
+++ scikit_mol/descriptors.py
@@ -9,30 +9,34 @@ from sklearn.base import BaseEstimator, TransformerMixin
 
 class Desc2DTransformer(BaseEstimator, TransformerMixin):
     def __init__(
-        self, use_all_descriptors: bool = True, custom_desc_list: Optional[str] = None
+        self, custom_desc_list: Optional[str] = None
     ):
-        self.use_all_descriptors = use_all_descriptors
+        #self.use_all_descriptors = use_all_descriptors
         self.desc_list = custom_desc_list
         self.calculators = self._get_desc_calculator()
 
     def _get_desc_calculator(self) -> MolecularDescriptorCalculator:
-        all_decriptors = [x[0] for x in Descriptors._descList]
-        if self.use_all_descriptors:
-            self.desc_list = all_decriptors
-        else: 
-            assert self.desc_list is not None, \
-                "please provide your preferred descriptor names, otherwise let use_all_descriptors == True"
-            self.desc_list = [desc for desc in self.desc_list if desc in all_decriptors]
+        if self.desc_list:
+            unknown_descriptors = [desc_name for desc_name in self.desc_list if desc_name not in self.available_descriptors]
+            assert (not unknown_descriptors), f"Unknown descriptor names {unknown_descriptors} specified, please check available_descriptors property\nPlease check availble list {self.available_descriptors}" 
+        else:
+            self.desc_list = self.available_descriptors 
         return MolecularDescriptorCalculator(self.desc_list)
 
-    def get_desc_names(self) -> List[str]:
-        # callable function to get list of descriptor names
+    @property
+    def available_descriptors(self) -> List[str]:
+        """Property to get list of all available descriptor names"""
+        return [descriptor[0] for descriptor in Descriptors._descList]
+
+    @property
+    def get_selected_descriptor_names(self) -> List[str]:
+        """Property to get list of the selected descriptor names"""
         return list(self.calculators.GetDescriptorNames())
 
     def _transform_mol(self, mol: Mol) -> List[Any]:
         return list(self.calculators.CalcDescriptors(mol))
 
-    def fit(self, x, y=None):
+    def fit(self, X, y=None):
         return self
 
     def transform(self, x: List[Mol], y=None) -> np.ndarray:
