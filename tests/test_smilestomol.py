1602d45c105464212c78142a21673f9f8d251ed9
Esben Bjerrm <esben@odysseytx.com>
2022-10-17 14:38:25 +0000

    Added a pytest for smilestomol

--- tests/test_smilestomol.py
+++ tests/test_smilestomol.py
@@ -1,43 +1,47 @@
 import pytest
 import numpy as np
 import pandas as pd
+from sklearn import clone
 from rdkit import Chem
 from scikit_mol.transformers import SmilesToMol
 
-smiles_list = ['O=C(O)c1ccccc1',
+
+@pytest.fixture
+def smiles_list():
+    return [Chem.MolToSmiles(Chem.MolFromSmiles(smiles)) for smiles in  ['O=C(O)c1ccccc1',
                 'O=C([O-])c1ccccc1',
                 'O=C([O-])c1ccccc1.[Na+]',
                 'O=C(O[Na])c1ccccc1',
-                'C[N+](C)C.O=C([O-])c1ccccc1']
+                'C[N+](C)C.O=C([O-])c1ccccc1']]
 
-def test_smilestomol():
-    transformer = SmilesToMol()
+@pytest.fixture
+def smilestomol_transformer():
+    return SmilesToMol()
 
-    mol_list = transformer.transform(smiles_list)
+def test_smilestomol(smiles_list, smilestomol_transformer):
+    mol_list = smilestomol_transformer.transform(smiles_list)
+    assert all([ a == b for a, b in zip(smiles_list, [Chem.MolToSmiles(mol) for mol in mol_list])])
+
+def test_smilestomol_numpy(smiles_list, smilestomol_transformer):
+    mol_list = smilestomol_transformer.transform(np.array(smiles_list))
 
     assert all([ a == b for a, b in zip(smiles_list, [Chem.MolToSmiles(mol) for mol in mol_list])])
 
-def test_smilestomol_numpy():
-    transformer = SmilesToMol()
-
-    mol_list = transformer.transform(np.array(smiles_list))
+def test_smilestomol_pandas(smiles_list, smilestomol_transformer):
+    mol_list = smilestomol_transformer.transform(pd.Series(smiles_list))
 
     assert all([ a == b for a, b in zip(smiles_list, [Chem.MolToSmiles(mol) for mol in mol_list])])
 
-def test_smilestomol_pandas():
-    transformer = SmilesToMol()
+def test_smilestomol_clone(smilestomol_transformer):
+    t2 = clone(smilestomol_transformer)
+    params   = smilestomol_transformer.get_params()
+    params_2 = t2.get_params()
+    assert all([ params[key] == params_2[key] for key in params.keys()])
 
-    mol_list = transformer.transform(pd.Series(smiles_list))
-
-    assert all([ a == b for a, b in zip(smiles_list, [Chem.MolToSmiles(mol) for mol in mol_list])])
-
-
-def test_smilestomol_unsanitzable():
-    transformer = SmilesToMol()
-
+def test_smilestomol_unsanitzable(smilestomol_transformer):
     smiles_list = ['Invalid']
     
     with pytest.raises(ValueError):
-        transformer.transform(smiles_list)
+        smilestomol_transformer.transform(smiles_list)
 
     
