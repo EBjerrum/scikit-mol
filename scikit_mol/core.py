0182e49a5b30aaa9c73221d8d00725faeea92f23
Enrico Gandini <enricogandini93@gmail.com>
2024-03-02 23:40:09 +0100

    refactor: remove no longer needed todo comment
    See GitHub discussion.

--- scikit_mol/core.py
+++ scikit_mol/core.py
@@ -11,10 +11,6 @@ def get_mols_from_X(X):
         """Get the molecules iterable from the input X"""
         if isinstance(X, pd.DataFrame):
             try:
-                # TODO: possibly handle the case in which a DataFrame
-                # contains multiple molecule columns (as if from a column selector transformer).
-                # In that case, the resulting array should be a concatenation of the fingerprint arrays
-                # for each molecule column.
                 # TODO: Change core logic of how scikit-mol transformers handle input:
                 # make them only accept 2D arrays with a single column (and possibly flat lists).
                 # See GitHub discussion.
