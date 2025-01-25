9af18bf8bf42e1bbb41cba7e15f89defb5fbeaf1
Anton Siomchen <41703271+asiomchen@users.noreply.github.com>
2025-01-25 16:46:51 +0100

    Enhance parallelized_with_batches function with detailed docstring

--- scikit_mol/parallel.py
+++ scikit_mol/parallel.py
@@ -6,11 +6,29 @@ from joblib import Parallel, delayed
 
 
 def parallelized_with_batches(
-    fn: Callable,
+    fn: Callable[[Sequence[Any]], Any],
     inputs_list: np.ndarray,
     n_jobs: Optional[int] = None,
     **job_kwargs: Any,
 ) -> Sequence[Optional[Any]]:
+    """
+    Execute a function in parallel with batches of inputs.
+    Parameters
+    ----------
+    fn : Callable[[Sequence[Any]], Any]
+        The function to be executed in parallel. It should accept a sequence of inputs.
+    inputs_list : np.ndarray
+        The list of inputs to be processed in parallel.
+    n_jobs : Optional[int], default=None
+        The number of jobs to run in parallel. If None, it will be determined automatically.
+    **job_kwargs : Any
+        Additional keyword arguments to pass to the Parallel object.
+    Returns
+    -------
+    Sequence[Optional[Any]]
+        A sequence of results from the function executed in parallel.
+    """
+
     pool = Parallel(n_jobs=n_jobs, **job_kwargs)
     n_jobs = pool._effective_n_jobs()
     if n_jobs > len(inputs_list):
