0c706108f0871fd26a9148a533f2b017bdee33af
Anton Siomchen <41703271+asiomchen@users.noreply.github.com>
2025-01-25 15:34:03 +0100

    add joblib to the standarizer

--- scikit_mol/standardizer.py
+++ scikit_mol/standardizer.py
@@ -1,7 +1,8 @@
 # A scikit-learn compatible molecule standardizer
 # Author: Son Ha
 
-import multiprocessing
+
+import functools
 
 import numpy as np
 from rdkit import Chem
@@ -15,15 +16,15 @@ from scikit_mol.core import (
     check_transform_input,
     feature_names_default_mol,
 )
+from scikit_mol.parallel import parallelized_with_batches
 
 
 class Standardizer(TransformerMixin, NoFitNeededMixin, BaseEstimator):
     """Input a list of rdkit mols, output the same list but standardised"""
 
-    def __init__(self, neutralize=True, parallel=False, safe_inference_mode=False):
+    def __init__(self, neutralize=True, parallel=None, safe_inference_mode=False):
         self.neutralize = neutralize
         self.parallel = parallel
-        self.start_method = None  # TODO implement handling of start_method
         self.safe_inference_mode = safe_inference_mode
 
     def fit(self, X, y=None):
@@ -69,40 +70,17 @@ class Standardizer(TransformerMixin, NoFitNeededMixin, BaseEstimator):
 
     @check_transform_input
     def transform(self, X, y=None):
-        if not self.parallel:
-            return self._transform(X)
-
-        elif self.parallel:
-            n_processes = (
-                self.parallel if self.parallel > 1 else None
-            )  # Pool(processes=None) autodetects
-            n_chunks = (
-                n_processes * 2
-                if n_processes is not None
-                else multiprocessing.cpu_count() * 2
-            )  # TODO, tune the number of chunks per child process
-
-            with multiprocessing.get_context(self.start_method).Pool(
-                processes=n_processes
-            ) as pool:
-                x_chunks = np.array_split(X, n_chunks)
-                parameters = self.get_params()
-                arrays = pool.map(
-                    parallel_helper,
-                    [
-                        (self.__class__.__name__, parameters, x_chunk)
-                        for x_chunk in x_chunks
-                    ],
-                )
-                arr = np.concatenate(arrays)
-            return arr
+        parameters = self.get_params()
+        func = functools.partial(parallel_helper, self.__class__.__name__, parameters)
+        arrays = parallelized_with_batches(func, X, self.parallel)
+        arr = np.concatenate(arrays)
+        return arr
 
 
-def parallel_helper(args):
+def parallel_helper(classname, parameters, X_mols):
     """Parallel_helper takes a tuple with classname, the objects parameters and the mols to process.
     Then instantiates the class with the parameters and processes the mol.
     Intention is to be able to do this in child processes as some classes can't be pickled"""
-    classname, parameters, X_mols = args
     from scikit_mol import standardizer
 
     transformer = getattr(standardizer, classname)(**parameters)
