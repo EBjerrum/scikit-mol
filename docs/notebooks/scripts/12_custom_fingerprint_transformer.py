8a4e2f127af76e4ffcf8a452038e6017ee92a835
Esben Jannik Bjerrum <esbenjannik@rocketmai.com>
2025-03-30 19:55:28 +0200

    Synced script to notebook

--- docs/notebooks/scripts/12_custom_fingerprint_transformer.py
+++ docs/notebooks/scripts/12_custom_fingerprint_transformer.py
@@ -1,11 +1,12 @@
 # ---
 # jupyter:
 #   jupytext:
+#     formats: ipynb,py:percent
 #     text_representation:
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.3'
-#       jupytext_version: 1.16.6
+#       jupytext_version: 1.16.1
 #   kernelspec:
 #     display_name: .venv
 #     language: python
